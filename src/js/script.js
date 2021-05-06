bd343039a0a59b7659ef2f7768006601d86dbc91
Vladislav <example@gmail.com>
2021-05-06 22:03:26 +0300

    finish form decoration

--- src/js/script.js
+++ src/js/script.js
@@ -49,4 +49,37 @@ $(document).ready(function(){
 			$('.overlay, #order').fadeIn('slow');
 		});
 	});
+
+	function valideForms(form){
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста, введите свое имя",
+					minlength: jQuery.validator.format("Введите больше {0} симболов!")
+				},
+				phone: "Пожалуйста, введите свой номер телефона",
+				email: {
+					required: "Пожалуйста, введите свой почтовый адрес",
+					email: "Неправильно введён почтовый адрес"
+				}
+			}
+		});
+	};
+	
+	valideForms('#consultation-form');
+	valideForms('#consultation form');
+	valideForms('#order form');
+	
+	$('input[name-phone]').mask("+7(999) 999-9999");
 });
