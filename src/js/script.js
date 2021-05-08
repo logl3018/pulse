96d253b094c01cd9c642ecfdcf250d1adee8714d
Vladislav <example@gmail.com>
2021-05-08 21:17:38 +0300

    create animations

--- src/js/script.js
+++ src/js/script.js
@@ -96,4 +96,22 @@ $(document).ready(function(){
         });
         return false;
     });
+
+	//Smooth scrol and pageup
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	/* $("a[href^='#']").click(function() {
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	}); */
+
+	new WOW().init();
 });
